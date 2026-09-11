9ad605b759c027fb5b7676e872dfb93f67fafc10
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2026-09-12 02:34:59 +0400

    feat(init.lua): Update shell default

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+vim.o.shell="/bin/zsh"
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
